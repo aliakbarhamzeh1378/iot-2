d5012f87c4f8919f784ee32aeb8408520bb503f9
rozhaaan <you@example.com>
2023-04-29 11:34:08 -0400

    add vrifytoken to token file

--- src/lib/token.js
+++ src/lib/token.js
@@ -41,6 +41,18 @@ class Token {
   }
 
 
+  async  verifyGoogleToken(token) {
+    try {
+      const ticket = await client.verifyIdToken({
+        idToken: token,
+        audience: process.env.GOOGLE_CLIENT_ID,
+      });
+      return { payload: ticket.getPayload() };
+    } catch (error) {
+      return { error: "Invalid user detected. Please try again" };
+    }
+  }  
+
 }
 
 module.exports = { Token };
