da5250965ba873f573cd7f78f2972ff89f20300a
Rozhin <rahmanirojin12@yahoo.com>
2023-05-15 19:23:49 +0330

    add role permission + add get data from app

--- src/routes/slave/slaveRouter.js
+++ src/routes/slave/slaveRouter.js
@@ -2,21 +2,22 @@ const express = require("express");
 const router = express.Router();
 const slaveController = require("./slaveController");
 const { MiddleWare } = require("../../lib/middleware")
+const Role = require("../../lib/roles_list")
 
 router.post("/slave",
-    [MiddleWare.checkToken, MiddleWare.emptyCheck],
+    [MiddleWare.checkToken, MiddleWare.emptyCheck , MiddleWare.verifyRoles(Role.ADMIN,Role.SUPERADMIN)],
     slaveController.add);
 
 router.delete("/slave",
-    [MiddleWare.checkToken]
+    [MiddleWare.checkToken , MiddleWare.verifyRoles(Role.SUPERADMIN)]
     , slaveController.delete)
 
 router.put("/slave",
-    [MiddleWare.checkToken]
+    [MiddleWare.checkToken ,MiddleWare.verifyRoles(Role.ADMIN ,Role.SUPERADMIN)]
     , slaveController.update);
 
-router.get("/slave",
-    [MiddleWare.checkToken]
-    , slaveController.data);
+router.post("/getdata",
+    [MiddleWare.checkToken ,MiddleWare.verifyRoles(Role.ADMIN ,Role.SUPERADMIN)] ,
+     slaveController.data);
 
 module.exports = router;
